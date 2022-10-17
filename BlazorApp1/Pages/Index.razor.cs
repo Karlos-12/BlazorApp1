5a13ec54bd5d7ae00cdf5f1fb21c2e8d08c8f75b
Karel Cveček <kastrolekkaja@seznam.cz>
2022-10-17 19:56:04 +0200

    sdf

---
+++ BlazorApp1/Pages/Index.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorApp1.Pages
+{
+    public partial class Index
+    {
+        public int count = 0;
+        public void lool()
+        {
+            count++;
+        }
+    
+
+
+
+
+        public class lolmec
+        {
+
+        }
+    }
+}
+
