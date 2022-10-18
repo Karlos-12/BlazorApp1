5f8537589cd36b48623ec5ee298c4793c1ef7ce9
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2022-10-18 10:39:24 +0200

    sdfasd

--- BlazorApp1/Pages/Index.razor.cs
+++ BlazorApp1/Pages/Index.razor.cs
@@ -1,14 +1,23 @@
-﻿namespace BlazorApp1.Pages
+﻿using Microsoft.JSInterop;
+
+namespace BlazorApp1.Pages
 {
     public partial class Index
     {
+        private readonly IJSRuntime js;
+
         public string id;
         public string pass;
         public void Trypass()
         {
             System.Console.WriteLine("ID: " + id + "\nPass: " + pass);
+            
         }
        
+        public void lol()
+        {
+           js.InvokeVoidAsync("PageScript");
+        }
     }
 }
 
---
+++ BlazorApp1/wwwroot/js/PageScript.js
@@ -0,0 +1,4 @@
+﻿function xd()
+{
+    console.log("lolik");
+}
